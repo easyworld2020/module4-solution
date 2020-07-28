dbbc85e292ded66c995286226e06d2312a6db827
Your Name <you@example.com>
2020-07-27 23:11:05 -0700

    module 5 - solution

--- src/data/menudata.service.js
+++ src/data/menudata.service.js
@@ -7,46 +7,31 @@
     MenuDataService.$inject = ['$http', 'ApiBaseUrl', 'ApiCategoriesPath', 'ApiItemsPath'];
     function MenuDataService($http, ApiBaseUrl, ApiCategoriesPath, ApiItemsPath) {
       var service = this;
-  
-      var compareNames = function (a, b) {
-        var aName = a.name.toLowerCase();
-        var bName = b.name.toLowerCase();
-  
-        if (aName < bName) {
-          return -1;
-        }
-        if (aName > bName) {
-          return 1;
-        }
-        return 0;
-      };
-  
+
+      //return a promise containing categories
       service.getAllCategories = function () {
         return $http({
           method: "GET",
           url: ApiBaseUrl + ApiCategoriesPath
         })
-        .then(function (response) {
-          console.log("Categories HTTP response", response);
-          return response.data.sort(compareNames);
+        .then((response) => {
+          return response.data;
         })
-        .catch(function (error) {
-          console.log("Something went wrong", error);
+        .catch((error) => {
         });
       };
-  
+
+    //return a promise containing items by categories
       service.getItemsForCategory = function (categoryShortName) {
         return $http({
           method: "GET",
           url: ApiBaseUrl + ApiItemsPath,
           params: {category: categoryShortName}
         })
-        .then(function (response) {
-          console.log("Items HTTP response", response);
-          return response.data.menu_items.sort(compareNames);
+        .then( (response) => {
+          return response.data.menu_items;
         })
-        .catch(function (error) {
-          console.log("Something went wrong", error);
+        .catch( (error) => {
         });
       };
     }
